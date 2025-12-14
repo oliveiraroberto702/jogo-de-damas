53881c2dcc1b0217f319049451bf5330fecf0584
oliveiraroberto702 <robertvascol702@gmail.com>
2025-12-14 18:00:43 -0300

    Primeira classe: Posicao

---
+++ src/principal/Programa.java
@@ -0,0 +1,11 @@
+package principal;
+
+import entidades.Posicao;
+
+public class Programa {
+	public static void main(String[] args) {
+		
+		Posicao posicao = new Posicao(10,20);
+		System.out.println(posicao);
+	}
+}
