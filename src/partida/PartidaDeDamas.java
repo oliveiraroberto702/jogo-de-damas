765cf46c4b0443a8db9ad05482f0a9b698adeb02
oliveiraroberto702 <robertvascol702@gmail.com>
2025-12-16 17:21:56 -0300

    Pequenas melhorias na montagem do tabuleiro

--- src/partida/PartidaDeDamas.java
+++ src/partida/PartidaDeDamas.java
@@ -25,8 +25,40 @@ public class PartidaDeDamas {
 		return mat;
     }
 	private void cargaInicial() {
-		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (2,1));
-		tabuleiro.colocaPeca(new Dama(tabuleiro, Cor.BLACK), new Posicao (0,4));
-		tabuleiro.colocaPeca(new Dama(tabuleiro, Cor.WHITE), new Posicao (7,4));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (0,0));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (0,1));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (0,2));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (0,3));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (0,4));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (0,5));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (0,6));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (0,7));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (1,0));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (1,1));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (1,2));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (1,3));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (1,4));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (1,5));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.WHITE), new Posicao (1,6));
+		tabuleiro.colocaPeca(new Dama(tabuleiro, Cor.WHITE), new Posicao (1,7));
+		
+		tabuleiro.colocaPeca(new Dama(tabuleiro, Cor.BLACK), new Posicao (6,0));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (6,1));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (6,2));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (6,3));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (6,4));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (6,5));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (6,6));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (6,7));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (7,0));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (7,1));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (7,2));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (7,3));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (7,4));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (7,5));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (7,6));
+		tabuleiro.colocaPeca(new Peao(tabuleiro, Cor.BLACK), new Posicao (7,7));
+		
+		
 	}
 }
--- src/partida/pecas/Dama.java
+++ src/partida/pecas/Dama.java
@@ -5,14 +5,15 @@ import partida.Cor;
 import partida.PecaDaPartida;
 
 public class Dama extends PecaDaPartida{
-
+    public static final String ANSI_CIANO_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_RESET = "\u001B[0m";
 	public Dama(Tabuleiro tabuleiro, Cor cor) {
 		super(tabuleiro, cor);
 	
 	}
 	@Override
 	public String toString() {
-		return "D";
+		return ANSI_CIANO_BACKGROUND + "D" + ANSI_RESET;
 	}
 
 }
